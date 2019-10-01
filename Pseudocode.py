4985d5af89241575050a7853e917f2fbe7e52fa4
lasyatirumandas <54901366+lasyatirumandas@users.noreply.github.com>
2019-10-01 09:29:06 -0600

    Introduction of game Pseudocode

---
+++ Pseudocode.py
@@ -0,0 +1,23 @@
+# Introducing Belle and her problem about the lost rose and the beast
+    # PRINT belle talking about her problem and asks you for help
+    # PRINT belle talkin about the rose and love that it represents in beast
+
+# Belle giving instruction
+    # PRINT I have already informed Robin Hood about your arrival and he will
+    # be waiting for you in Sherwood Forest
+        # PRINT belle talks about the 3 realms you must travel through
+        # PRINT belle talks about the items to be found in each realm
+            # PRINT Enchanted Forest- Sword
+            # PRINT Sherwood Forest- Robin's bow
+            # PRINT Conques, France- Magic Bean
+            # (the rose will be in the Enchanted Forest)
+        # The sword will help you fight the beast
+        # The Bow is Robin's friendship, so you need to bring him along
+        # The magic realm is needed to go to the Enchanted Forest
+
+# PRINT Belle talks about the supplies that you can look for or have along
+# the way, but needed:
+    # Food, Water, Magic Wand
+
+# PRINT you are in Conques, France right now and i will give you a
+# magic bean, so you can travel to the enchanted forest. 
