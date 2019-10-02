49451caf7c79e0fb40ac0dfa983c55afbd2e1b24
lasyatirumandas <54901366+lasyatirumandas@users.noreply.github.com>
2019-10-02 09:31:25 -0600

    Updated Pseudocode

--- Pseudocode.py
+++ Pseudocode.py
@@ -13,11 +13,49 @@
             # (the rose will be in the Enchanted Forest)
         # The sword will help you fight the beast
         # The Bow is Robin's friendship, so you need to bring him along
-        # The magic realm is needed to go to the Enchanted Forest
+        # The magic bean is needed to travel
 
 # PRINT Belle talks about the supplies that you can look for or have along
 # the way, but needed:
     # Food, Water, Magic Wand
 
-# PRINT you are in Conques, France right now and i will give you a
-# magic bean, so you can travel to the enchanted forest. 
+# PRINT you are in Conques, France right now and i will give you max of 2
+# magic beans use them wisely.
+
+# If you want to go to the Enchnated forest, type Right
+# If you want to go to the Sherwood forest, type Left
+
+# to Enchanted Forest:
+    # PRINT You are in the Enchanted forest and you have found a sword on the
+    # ground
+    # If you want to go right, type right
+        # You have come across an orge and you have to fight
+            # If you want to attack, type attack
+            # If you want to defend, type defend
+            # If you want to run, type run
+                 # (2 attacks and the orge is dead)
+    # If you want to go Left, type Left
+        # PRINT nothing happens
+    # If you want to go up, type up
+        # PRINT nothing happens
+    # If you want to go down, type down
+        # PRINT THE ROSE
+
+# to Sherwood Forest:
+    # PRINT You are in the Sherwood forest and Robin Hood greets you
+    # have to make an alliance with him
+        # PRINT I heard from Belle that you assistance with something
+            # type Yes
+        # PRINT Do you need help holding off the beast?
+            # type Yes
+        # PRINT I am willing to come with you
+            # type amazing!!
+    # Now you got Robin's Bow
+    # on the other side of the side, you hear villagers whispering abotu the
+    # red rose, It might be the one you are looking for
+        # PRINT I heard that the red rose to heal the beast is in the
+        # Enchanted forest.
+        # PRINT Let's got to the Enchanted Forest
+
+# to Conques, France:
+    #
