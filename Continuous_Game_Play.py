bbdf95e8a836724a7f369d21a880be7c488d6e41
lasyatirumandas <54901366+lasyatirumandas@users.noreply.github.com>
2019-11-07 09:31:20 -0600

    update continuous_game_play

--- Continuous_Game_Play.py
+++ Continuous_Game_Play.py
@@ -26,12 +26,55 @@ def play():
     # print list of valid actions
     print(Actions)
     while True:
-        action_input = get_input("Action: ")
+        action_input = get_command("Action: ")
         if action_input in Actions:
             print(f"{action_input.title()}")
             if action_input == 'Quit':
-                sys.exit()
-            elif action_input
+                break
+            elif action_input == 'Places':
+                choose_Enchanted_Forest()
+                choose_Sherwood_Forest()
+                choose_Conques_France()
+        else:
+            print("Invalid action")
+
+# defining get command function for player input
+def get_command(message):
+    action_input = input(message)
+    return action_input.lower()
+
+# defining enchnated forest
+def choose_Enchanted_Forest():
+    print("You are in the Enchanted forest and you have found a sword on the")
+    print("ground and now that is yours.")
+    print(Directions)
+    while True:
+        direction_input = get_command('What direction?')
+        if direction_input in Directions:
+            print(f"{direction_input.title()}")
+            elif direction_input == 'Left':
+                print(f"Moving Left")
+                print("Nothing is here")
+            elif direction_input == 'Right':
+                print(f"Moving Right")
+                print("The orge is sleeping and you will let it sleep.")
+            elif direction_input == 'Forward':
+                print(f"Moving Forward")
+                print("Nothing is here")
+            elif direction_input == 'Backward':
+                print(f"Moving Backward")
+                print(f"You have found the Rose")
+        else:
+            print("Invalid Direction")
+
+def choose_Sherwood_Forest():
+    print("You are in the Sherwood forest and Robin Hood greets you.")
+    print("Robin Hood and you have become alliances in this adventure.")
+    print("Robin: I heard from Belle that you need assistance with something")
+    print("and I will help you hold off the beast and I am willing to come") 
+    print("with you")
+
+
 
 
 play()
