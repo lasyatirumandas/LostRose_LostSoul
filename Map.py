8453986adb7f75243fb01a428af42902806cc8c8
lasyatirumandas <54901366+lasyatirumandas@users.noreply.github.com>
2019-11-19 09:26:06 -0600

    update map.py

--- Map.py
+++ Map.py
@@ -2,3 +2,13 @@
 # CS30 P1
 # Nov 18, 2019
 # Map
+
+
+land_map = [
+        [Enchanted_nothing(0, ), Enchanted_orge(1, 0), Sherwood_nothing(2, 0), Sherwood_nothing(3, 0)],
+        [Enchanted_nothing(0, 1), Enchanted_rose(1, 1), Sherwood_robin(2, 1), Sherwood_nothing(2, 1),]
+        [Conques_belle, Beast_case, Beast_beast],
+        [Beast_nothing, Beast_nothing]
+]
+land_map = []
+print(land_map)
