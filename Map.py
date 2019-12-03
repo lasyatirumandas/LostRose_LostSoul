fb3c13bd7ae32a1de8f09ca1e5025eff08698b60
lasyatirumandas <54901366+lasyatirumandas@users.noreply.github.com>
2019-12-03 14:33:52 -0600

    update map.py

--- Map.py
+++ Map.py
@@ -3,17 +3,25 @@
 # Nov 18, 2019
 # Map
 
-
+# Make a list of all the places on the map
 land_map = [
-        ['Enchanted_forward', 'Enchanted_right', 'Sherwood_forward', 'Sherwood_right'],
-        ['Enchanted_left', 'Enchanted_backward', 'Sherwood_left', 'Sherwood_backward'],
-        ['               ', 'Conques_belle', 'Beast_forward', 'Beast_right'],
-        ['               ', '             ', 'Beast_left', 'Beast_backward']
-]
+            ['Enchanted_forward', 'Enchanted_right', 'Sherwood_forward',
+             'Sherwood_right'],
+            ['Enchanted_left', 'Enchanted_backward', 'Sherwood_left',
+             'Sherwood_backward'],
+            ['               ', 'Conques_belle', 'Beast_forward',
+             'Beast_right'],
+            ['               ', '             ', 'Beast_left',
+             'Beast_backward']
+            ]
 
 
+# make a function for printing the map
 def land_maps():
+    """Printing the map that looks above"""
     for rows in land_map:
         print(rows)
-
+        
+        
+# Print the land_map
 print(land_maps())
