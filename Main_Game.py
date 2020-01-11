c69c06fa4b0a8d4c305e69e6184c9f97ed1a29b1
lasyatirumandas <54901366+lasyatirumandas@users.noreply.github.com>
2020-01-10 19:53:21 -0600

    update

--- Main_Game.py
+++ Main_Game.py
@@ -154,9 +154,9 @@ tower. All she wants to do is to be free and find her parents.""")
             print("""
 From the top of the tower, Rapunzel hears you and answers back.
 RAPUNZEL: Who are you? Where are you from? I can't help you because I am
-locked in this tower. You might wonder why and I wish I could tell you, but I
-don't know. I really want to find my parents, though. I have never seem them.
-""")
+          locked in this tower. You might wonder why and I wish I could
+          tell you, but I don't know. I really want to find my parents,
+          though. I have never seem them.""")
             # input for talking to rapunzel
             for Inputs2 in Input2:
                 print(f"Inputs: {Inputs2}")
@@ -165,8 +165,8 @@ don't know. I really want to find my parents, though. I have never seem them.
                 if answer_input == 'Quit':
                     sys.exit()
                 elif answer_input == "I will help you, come down" or "We can find them together":
-                    print("""RAPUNZEL: Alright! I am coming down.""")
                     print("""
+RAPUNZEL: Alright! I am coming down.
 After she comes down, both of you continue on your path. Walking along a path,
 both of you look around to find clues. You find a Tavern and ask Rapunzel to
 come in with you.""")
@@ -178,29 +178,22 @@ come in with you.""")
                         if answer_input == 'Quit':
                             sys.exit()
                         elif answer_input == "Come on let's go in" or "You never know what we will find, come on.":
-                            print("""RAPUNZEL: But I don't think we will find
-                                               anything here because it is a
-                                               tavern. Annyway, I will come
-                                               along because there might be a
-                                               chance.""")
-                            print("""They both enter the tavern just to get
-                                  stared at by a bunch of drunk viking thugs.
-                                  VIKING 1: Who are you?
-                                  VIKING 2: What are you doing here?
-                                  RAPUNZEL: I am sorry for intruding, but we
-                                            were wondering if you can give us
-                                            some information. I have lost my
-                                            parents when I was just a little
-                                            girl & I was taken away by Mother
-                                            Gothel, who locked me away in a
-                                            tower. I'm looking for my parents
-                                            and I was wondering if you knew
-                                            anyone.
-                                  VIKING 1: We have this picture if it helps.
-                                  The viking hands the picture to Rapunzel,
-                                  who looks stunned.
-                                  VIKING 2: Is this it?
-                                  Rapunzel: Ya! This is it!! That is me.""")
+                            print("""
+RAPUNZEL: But I don't think we will find anything here because it is a
+          tavern. Annyway, I will come along because there might be a
+          chance.
+They both enter the tavern just to get stared at by a bunch of drunk
+viking thugs.
+VIKING 1: Who are you?
+VIKING 2: What are you doing here?
+RAPUNZEL: I am sorry for intruding, but we were wondering if you can give us
+          some information. I have lost my parents when I was just a little
+          girl & I was taken away by Mother Gothel, who locked me away in a
+          tower. I'm looking for my parents, so do you know anything.
+VIKING 1: We have this picture if it helps. The viking hands the picture
+          to Rapunzel, who looks stunned.
+VIKING 2: Is this it?
+Rapunzel: Ya! This is it!! That is me.""")
                             # input for finding the map of corona
                             for Inputs4 in Input4:
                                 print(f"Inputs: {Inputs4}")
@@ -209,37 +202,20 @@ come in with you.""")
                                 if answer_input == 'Quit':
                                     sys.exit()
                                 elif answer_input == 'Perfect! Where are they?' or 'Yess we did it Rapunzel!! Where can we find them?':
-                                    print("""VIKING 1: They live in Corona.
-                                                       Did you know that you
-                                                       are the daughter of
-                                                       King Frederic and
-                                                       Queen Arianna.
-                                             RAPUNZEL: What?! I'm a PRINCESS!
-                                                       How do we go there?
-                                             VIKING 2: Keep going straight
-                                                       and you will see a
-                                                       bridge. You have to
-                                                       cross the bridge to
-                                                       get to Corona.""")
-                                    print("""And so you and Rapunzel came out
-                                          of the tavern and continued on
-                                          their path.
-                                          After awhile, you see the bridge,
-                                          so you and Rapunzel cross the bidge.
-                                          """)
-                                    print("""When Rapunzel saw a stranger in
-                                          the town of Corona, she went up to
-                                          them and started to question.
-                                          RAPUNZEL: Hi! Do you know where I
-                                                    can find my parents... I
-                                                    mean King Frederick and
-                                                    Queen Arianna?
-                                          STRANGER: I am not sure exactly
-                                                    where they would be, but
-                                                    check the homes.
-                                                    Here is a map.
-                                          RAPUNZEL: Thank you so much!!
-                                          """)
+                                    print("""
+VIKING 1: They live in Corona. Did you know that you are the daughter of
+          King Frederic and Queen Arianna.
+RAPUNZEL: What?! I'm a PRINCESS! How do we go there?
+VIKING 2: Keep going straight and you will see a bridge. You have to
+          cross the bridge to get to Corona.
+And so you and Rapunzel came out of the tavern and continued on their
+path. After awhile, you see the bridge, so both cross the bidge.
+When Rapunzel saw a stranger in the town of Corona, she went up to
+them and started to question.
+RAPUNZEL: Hi! Do you know where I can find my parents... I mean
+King Frederick and Queen Arianna?
+STRANGER: I am not sure, but check the homes. Here is a map.
+RAPUNZEL: Thank you so much!! """)
                                     # printing the map here
                                     Mapss = Map1("Corona Map")
                                     Mapss.printing_maps()
@@ -276,23 +252,22 @@ The ceremony has come to an end. You can go up to them now.""")
             if answer_input == 'Quit':
                 sys.exit()
             elif answer_input == 'Good Evening, Your Majesty'or 'Hello, King and Queen':
-                print("""KING: Good Evening, May I know Who you are?
-                      Rapunzel slowly pulls out the picture when she was
-                      little and shows it to the King and the Queen.
-                      After a long stare at the picture, they both look
-                      back at Rapunzel.""")
+                print("""
+KING: Good Evening, May I know Who you are?
+Rapunzel slowly pulls out the picture when she was little and shows it
+to the King and the Queen. After a long stare at the picture, they both
+look back at Rapunzel.""")
                 for Inputs7 in Input7:
                     print(f"Inputs: {Inputs7}")
                 answer_input = get_command("Response: ")
                 if answer_input == 'Quit':
                     sys.exit()
                 elif answer_input == 'She is your daughter' or 'Look at the similarities':
-                    print("""QUEEN: My daughter! Oh my my!! You are
-                                    Rapunzel, my daughter!!!
-                             RAPUNZEL: Mom. Mom, I'm so glad I'm here.
-                             KING: Rapunzel, so are we, my darling. """)
-                    print("""Rapunzel and her family lived happily ever after!
-                    """)
+                    print("""
+QUEEN: My daughter! Oh my my!! You are Rapunzel, my daughter!!!
+RAPUNZEL: Mom. Mom, I'm so glad I'm here.
+KING: Rapunzel, so are we, my darling.
+Rapunzel and her family lived happily ever after!""")
                     # inputs for exiting the game
                     for Inputs8 in Input8:
                         print(f"Inputs: {Inputs8}")
